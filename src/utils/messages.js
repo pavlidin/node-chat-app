a8c73fcfcf518d3dcdfb7a9984b02464342eac5c
Nikos Pavlidis <nikospavlidismail@gmail.com>
2021-04-11 01:30:39 +0300

    minor fixes

--- src/utils/messages.js
+++ src/utils/messages.js
@@ -2,19 +2,19 @@ const generateMessage = (username, text) => {
     return {
         username,
         text,
-        createdAt: new Date().getTime()
-    }
-}
+        createdAt: new Date().getTime(),
+    };
+};
 
 const generateLocationMessage = (username, url) => {
     return {
         username,
         url,
-        createdAt: new Date().getTime()
-    }
-}
+        createdAt: new Date().getTime(),
+    };
+};
 
 module.exports = {
     generateMessage,
-    generateLocationMessage
-}
+    generateLocationMessage,
+};
